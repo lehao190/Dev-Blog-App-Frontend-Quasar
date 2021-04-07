37214436a3b267030f1476e97b48d89eafe7ef18
lehao190 <leminhhao2014@yahoo.com>
2021-04-07 13:32:00 +0700

    Homepage layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,6 +3,9 @@
     <q-header reveal elevated class="bg-primary text-white">
       <q-toolbar class="main-wrapper text-black">
         <q-toolbar-title class="q-py-sm q-gutter-md">
+          <!-- Display on lesser than medium size only -->
+          <q-btn class="lt-md" dense flat round icon="menu" @click="left = !left" />
+
           <q-avatar
             class="bg-black text-white"
             icon="fab fa-blogger"
@@ -11,22 +14,37 @@
           />
           <!-- Searching for posts -->
           <q-input
-            class="absolute"
+            class="absolute gt-sm"
             style="width: 400px; display: inline-block; top: -5px;"
             filled
             dense
             placeholder="Tìm kiếm..."
           />
         </q-toolbar-title>
+
+        <!-- Login/Signup -->
         <div class="q-gutter-md">
+          <!-- Display on greater than medium size only -->
           <q-btn
-            class="text-weight-bold"
+            class="text-weight-bold gt-sm"
             flat
             type="a"
             label="Đăng Nhập"
             text-color="blue-8"
             no-caps
           />
+
+          <!-- Display on lesser than medium size only -->
+          <q-btn
+            class="text-weight-bold lt-md"
+            flat
+            type="a"
+            to="/search"
+            icon="fas fa-search"
+            text-color="dark"
+            no-caps
+          />
+
           <q-btn
             class="text-weight-bold"
             color="secondary"
@@ -42,17 +60,9 @@
     <!-- left sidebar when in mobile mode -->
     <q-drawer v-model="left" side="left">
       <!-- drawer content -->
-      aha Take on Me aha Take on Me aha Take on Me aha Take on Me aha Take on Me
-      aha Take on Me aha Take on Me aha Take on Me
+      <IntroSec/>
+      <UserNav/>
     </q-drawer>
-
-    <!-- right sidebar when in mobile mode -->
-    <q-drawer v-model="right" side="right">
-      <!-- drawer content -->
-      Nice Mate! great Nice Mate! great Nice Mate! great Nice Mate! great Nice
-      Mate! great Nice Mate! great
-    </q-drawer>
-
     <!-- Main content -->
     <q-page-container>
       <router-view />
@@ -61,11 +71,17 @@
 </template>
 
 <script>
+import IntroSec from '../components/home_page/IntroSec'
+import UserNav from '../components/home_page/UserNav'
+
 export default {
+  components: {
+    IntroSec,
+    UserNav
+  },
   data () {
     return {
-      left: false,
-      right: false
+      left: false
     }
   }
 }
